8fc92db66bdc6110eacbefed9094621ff88cea9f
SoybalAhmmed <soybalahmed@gmail.com>
2022-05-09 17:46:29 +0600

    banner added

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Service.css';
 
 const Service = ({service}) => {
-    const {name, img, description, price} = service;
+    const {name, img, description, price,quantity,supplier} = service;
     return (
         <div className='service'>
             <img className='w-100' src={img} alt="" />
             <h2>{name}</h2>
             <p>Price: {price}</p>
+            <p>quantity:{quantity}</p>
+            <p>supplier name:{supplier}</p>
             <p><small>{description}</small></p>
             <button className='btn btn-primary'>Book: {name}</button>
         </div>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
+import './Services.css';
 
 const Services = () => {
     const [services, setServices] = useState([]);
@@ -11,7 +12,8 @@ const Services = () => {
     }, [])
     return (
         <div>
-            <h2>Services:{services.length}</h2>
+            <h2>Our Services</h2>
+            <div className="services-container">
             {
                 services.map(service => <Service
                     key={service.id}
@@ -19,6 +21,7 @@ const Services = () => {
                 >
                 </Service>)
             }
+        </div>
         </div>
     );
 };

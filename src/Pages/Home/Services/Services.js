ec91cf514837d9fada1cabf618fc1dd38cde34ed
SoybalAhmmed <soybalahmed@gmail.com>
2022-05-09 17:58:54 +0600

    navbar added

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -11,18 +11,20 @@ const Services = () => {
         .then(data => setServices(data));
     }, [])
     return (
-        <div>
-            <h2>Our Services</h2>
-            <div className="services-container">
-            {
-                services.map(service => <Service
-                    key={service.id}
-                    service={service}
-                >
-                </Service>)
-            }
+        <div className='container'>
+        <div className="row">
+        <h1 className='text-primary text-center mt-5'> Our Services</h1>
+        <div className="services-container">
+        {
+            services.map(service => <Service
+                key={service.id}
+                service={service}
+            >
+            </Service>)
+        }
         </div>
         </div>
+    </div>
     );
 };
 
